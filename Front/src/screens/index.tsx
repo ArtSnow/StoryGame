606e7110d8b3054d1e5ad96d0c5cb2d582984d29
Chapliy Vladislav <vladislav.chapliy@altcraft.com>
2023-09-10 16:57:36 +0300

    replace null by component

--- Front/src/screens/index.tsx
+++ Front/src/screens/index.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import LoginScreen from '../screens/LoginScreen';
 import { GeneralStore } from './store';
 import MasterScreen from './MasterScreen';
+import PlayerScreen from './PlayerScreen';
 
 const MainComponent: React.FC = () => {
     const generalStore = useRef(new GeneralStore()).current;
@@ -16,7 +17,7 @@ const MainComponent: React.FC = () => {
         return <MasterScreen getId={generalStore.getId} getPassword={generalStore.getPassword} />;
     }
 
-    return null;
+    return <PlayerScreen getId={generalStore.getId} getPassword={generalStore.getPassword} />;
 };
 
 export default MainComponent;
